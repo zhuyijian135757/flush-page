49a2605270cec01a354610d18d73c4ded3641e04
zhuyijian135757 <zhuyijian135757@163.com>
2016-05-08 20:10:46 +0800

    bai du

---
+++ flush-page/src/main/java/net/flyingfat/page/BaiduAdsDomain.java
@@ -0,0 +1,178 @@
+package net.flyingfat.page;
+
+public class BaiduAdsDomain {
+
+	private String error_code;
+	
+	private String RequestId;
+	
+	private String osType;
+	
+	private String UUid;
+	
+	private String ads_key;
+	
+	private String imei;
+	
+	private String CreativeType;
+	
+	private String InteractionType;
+	
+	private String[] WinNoticeUrl;
+	
+	private String ClickUrl;
+	
+	private String Title;
+	
+	private String statusCode;
+	
+	private String Description;
+	
+	private String IconSrc;
+	
+	private String ImageSrc;
+	
+	private String AppPackage;
+	
+	private String AppSize;
+
+	public String getError_code() {
+		return error_code;
+	}
+
+	public void setError_code(String error_code) {
+		this.error_code = error_code;
+	}
+
+	public String getRequestId() {
+		return RequestId;
+	}
+
+	public void setRequestId(String requestId) {
+		RequestId = requestId;
+	}
+
+	public String getOsType() {
+		return osType;
+	}
+
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+
+	public String getUUid() {
+		return UUid;
+	}
+
+	public void setUUid(String uUid) {
+		UUid = uUid;
+	}
+
+	public String getAds_key() {
+		return ads_key;
+	}
+
+	public void setAds_key(String ads_key) {
+		this.ads_key = ads_key;
+	}
+
+	public String getImei() {
+		return imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public String getCreativeType() {
+		return CreativeType;
+	}
+
+	public void setCreativeType(String creativeType) {
+		CreativeType = creativeType;
+	}
+
+	public String getInteractionType() {
+		return InteractionType;
+	}
+
+	public void setInteractionType(String interactionType) {
+		InteractionType = interactionType;
+	}
+
+	public String[] getWinNoticeUrl() {
+		return WinNoticeUrl;
+	}
+
+	public void setWinNoticeUrl(String[] winNoticeUrl) {
+		WinNoticeUrl = winNoticeUrl;
+	}
+
+	public String getClickUrl() {
+		return ClickUrl;
+	}
+
+	public void setClickUrl(String clickUrl) {
+		ClickUrl = clickUrl;
+	}
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public String getIconSrc() {
+		return IconSrc;
+	}
+
+	public void setIconSrc(String iconSrc) {
+		IconSrc = iconSrc;
+	}
+
+	public String getImageSrc() {
+		return ImageSrc;
+	}
+
+	public void setImageSrc(String imageSrc) {
+		ImageSrc = imageSrc;
+	}
+
+	public String getAppPackage() {
+		return AppPackage;
+	}
+
+	public void setAppPackage(String appPackage) {
+		AppPackage = appPackage;
+	}
+
+	public String getAppSize() {
+		return AppSize;
+	}
+
+	public void setAppSize(String appSize) {
+		AppSize = appSize;
+	}
+	
+	
+	
+	
+}
